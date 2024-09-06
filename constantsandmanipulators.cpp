149ceea1e1eb0b5558f1e755d9b5edc83b8405c4
Shubham Suryawanshi <shubhamanils2003@gmail.com>
2024-09-06 15:11:38 +0530

    Constants and Manipulators

---
+++ constantsandmanipulators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int msin(){
+    int a=34;
+cout<<"The value of a was :"<<a;
+a=45;
+cout<<"The value of a is :"<<a;
+
+//...............Constants in c++..................//
+
+// const int a=3;
+// cout<<"The value of a was  :"<<a<<endl;
+// a=45;
+// cout<<"The value of a is  :"<<a<<endl;
+
+
+return 0;
+
+}
